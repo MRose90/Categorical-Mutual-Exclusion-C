53bedf1b038c0701899ad5794585cb38119d4e97
Michael Rose <rosem2@oregonstate.edu>
2016-11-08 13:03:13 -0800

    Basic Setup
    Created file with blank main

---
+++ mutual_exclusion.c
@@ -0,0 +1,12 @@
+//Michael Rose - Matthew Zakrevsky
+//Oregon State University - CS 444 - Concurrency Assignment 3
+//Categorical Mutual Exclusion
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
